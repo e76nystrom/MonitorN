540c6416b4e089893f11e54d4ed6e676df31f420
Eric Nystrom <eric.nystrom@yahoo.com>
2022-06-08 13:51:19 -0400

    Modify EclipseCPP/Monitor to make MonitorN.

---
+++ include/variant.h
@@ -0,0 +1,21 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2020-2021, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+#pragma once
+
+//#include VARIANT_H
+//#if defined(ARDUINO_NUCLEO_F103RB)
+//#include "variant_NUCLEO_F103RB.h"
+#include "C:/Users/Eric/.platformio/packages/framework-arduinoststm32/variants/STM32F1xx/F103R(8-B)T/variant_NUCLEO_F103RB.h"
+//#else
+//#include "variant_PILL_F103Cx.h"
+//#endif  /* STM32F103xB */
